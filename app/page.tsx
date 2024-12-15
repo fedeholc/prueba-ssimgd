d43ea03bc68b36cae4c413f206d7f2ae559edcfc
Federico Holc <federicoholc@gmail.com>
2024-12-15 12:49:03 -0300

    Add loading notification feature: implement Notify component with busy and done states, and update styles for loading animations

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,21 @@
 import styles from "./page.module.css";
 import Link from "next/link";
 import EmojiSpinnerButton from "./ButtonEmojiSpinner";
+import { useState } from "react";
+
+function useNotify() {
+  const [notifyState, setNotifyState] = useState<
+    "busy" | "done" | "error" | null
+  >(null);
+
+  const notify = {
+    setBusy: () => setNotifyState("busy"),
+    setDone: () => setNotifyState("done"),
+    setError: () => setNotifyState("error"),
+    getState: notifyState,
+  };
+  return { notify };
+}
 
 export default function Home() {
   const handleButtonClick = async (): Promise<void> => {
@@ -10,11 +25,18 @@ export default function Home() {
     return new Promise((resolve) => setTimeout(resolve, 2000));
   };
 
+  const { notify } = useNotify();
+
+  const [notifyState, setNotifyState] = useState<
+    "busy" | "done" | "error" | null
+  >(null);
+
   return (
     <div className={styles.page}>
       <div>
         {" "}
-        Dashboard <Link href="/dash">Dash</Link>
+        Dashboard <Link href="/dash">Dash</Link>{" "}
+        <Notify state={notify.getState} />
         <EmojiSpinnerButton
           emoji="🚀"
           label="Launch"
@@ -22,6 +44,27 @@ export default function Home() {
           onClick={handleButtonClick}
         />
       </div>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <button
+          onClick={() => {
+            notify.setBusy();
+            setTimeout(() => notify.setDone(), 2000);
+          }}
+        >
+          aaa
+        </button>
+        <Notify state={notifyState} />
+      </div>
     </div>
+  );
+}
+
+function Notify({ state }: { state: "busy" | "done" | "error" | null }) {
+  return (
+    <span className={`${styles[state || ""]}`}>
+      {!state && ""}
+      {state === "busy" && "loading..."}
+      {state === "done" && "ready..."}
+    </span>
   );
 }

f12f76c2d4cf2c9bd22fadd598fcf4d38e238e98
Federico Holc <federicoholc@gmail.com>
2024-12-16 20:08:01 -0300

    Add TODO comment to prevent adding new source if one is unsaved in Dash component

--- app/dash/page.tsx
+++ app/dash/page.tsx
@@ -10,7 +10,6 @@ export default function Dash() {
   const [sourceList, sourceListDispatch] = useReducer(sourcesListReducer, []);
   const [selecteditem, setSelectedItem] = useState<string>("");
   const [loadingMessage, setLoadingMessage] = useState<string>("");
-
   const { notify, notifyState } = useNotify3();
 
   useEffect(() => {
@@ -51,6 +50,7 @@ export default function Dash() {
   // adds a new source to the list, selects it
   // the new source is not saved to the database until the user clicks "Save"
   // the new source is identified by the _id "0"
+  //TODO: no permitir sumar uno nuevo si aun hay uno nuevo abierto sin guardar
   function handleNewSource() {
     sourceListDispatch({
       type: "add",
